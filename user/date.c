39ad57b8778d8e5fa3c964315e1f9b414a92d43b
Dylan Smith <dds00002@mix.wvu.edu>
2023-01-23 18:16:46 -0500

    Fixed date bounds

--- user/date.c
+++ user/date.c
@@ -24,21 +24,21 @@ void date(char *args) {
             return;
         }
         if (date[0] < 1 || date[0] > 12) {
-            println("Invalid month. Use 0-12");
+            println("Invalid month. Use 1-12");
             return;
         }
         if (date[1] < 1 || date[1] > 31) {
-            println("Invalid day. Use 0-31");
+            println("Invalid day. Use 1-31");
             return;
         }
         if (date[0] == 2 && date[1] > 28) {
-            println("Invalid date. Use 0-28 for days of February");
+            println("Invalid date. Use 1-28 for days of February");
         }
         if ((date[0] == 4 || date[0] == 6 || date[0] == 9 || date[0] == 11) && date[1] > 30) {
-            println("Invalid date. Use 0-30 for days of month given");
+            println("Invalid date. Use 1-30 for days of month given");
         }
         if ((date[0] == 1 || date[0] == 3 || date[0] == 5 || date[0] == 7 || date[0] == 8 || date[0] == 10 || date[0] == 12) && date[1] > 31) {
-            println("Invalid date. Use 0-31 for days of month given");
+            println("Invalid date. Use 1-31 for days of month given");
         }
         if (date[2] < 1970 || date[2] > 2023) {
             println("Invalid year. Use 1970-2023");
