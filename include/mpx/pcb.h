502b7245590c5ab4b514fbe949784e9907580a02
Nathan Fielding <nwf00001@mix.wvu.edu>
2023-02-10 09:42:08 -0500

    wrote pcb show and updated misc stuff

--- include/mpx/pcb.h
+++ include/mpx/pcb.h
@@ -38,6 +38,13 @@ pcb *pcb_allocate();
 int pcb_free(pcb *p);
 
 /**
+ * Frees the memory associated with a list
+ * @param head the head of the list to free
+ * @return 0 on success, -1 on failure
+ */
+int list_free(pcb *head);
+
+/**
  * Finds an element in a specified list by name
  * @param head the list to search
  * @param name the name of the pcb to find
