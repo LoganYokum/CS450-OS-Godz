e008c9cfc7f920952b9d08443be6c7c74fd393de
Logan Yokum <lgy00001@mix.wvu.edu>
2023-03-03 09:13:03 -0500

    loadr3 updates.

--- user/loadr3.c
+++ user/loadr3.c
@@ -5,13 +5,13 @@
 #include <mpx/call.h>
 #include <stdint.h>
 #include <stddef.h>
-
+//save stack_top into esp
 void loadr3(){
     // Create 5 system processes with different priorities.
     pcb *process1 = pcb_setup("proc1", 0, 0);
     context *p1 = (context*)(process1->stack_top);
-    p1->eax = (uint32_t)process1->stack_ptr;
-    p1->ebp = (uint32_t)process1->stack_ptr - (uint32_t)process1->stack_top;
+    p1->eax = (uint32_t)0;
+    p1->ebp = (uint32_t)process1->stack_top;
     p1->eip = (uint32_t)proc1;
     p1->eflags = (uint32_t)0x0202;
     p1->cs = (uint32_t)0x08;
@@ -29,8 +29,8 @@ void loadr3(){
 
     pcb *process2 = pcb_setup("proc2", 1, 1);
     context *p2 = (context*)(process2->stack_top);
-    p2->eax = (uint32_t)process2->stack_ptr;
-    p2->ebp = (uint32_t)process2->stack_ptr - (uint32_t)process2->stack_top;
+    p2->eax = (uint32_t)0;
+    p2->ebp = (uint32_t)process2->stack_top;
     p2->eip = (uint32_t)proc2;
     p2->eflags = (uint32_t)0x0202;
     p2->cs = (uint32_t)0x08;
@@ -48,8 +48,8 @@ void loadr3(){
 
     pcb *process3 = pcb_setup("proc3", 1, 2);
     context *p3 = (context*)(process3->stack_top);
-    p3->eax = (uint32_t)process3->stack_ptr;
-    p3->ebp = (uint32_t)process3->stack_ptr - (uint32_t)process3->stack_top;
+    p3->eax = (uint32_t)0;
+    p3->ebp = (uint32_t)process3->stack_top;
     p3->eip = (uint32_t)proc3;
     p3->eflags = (uint32_t)0x0202;
     p3->cs = (uint32_t)0x08;
@@ -67,8 +67,8 @@ void loadr3(){
 
     pcb *process4 = pcb_setup("proc4", 1, 3);
     context *p4 = (context*)(process4->stack_top);
-    p4->eax = (uint32_t)process4->stack_ptr;
-    p4->ebp = (uint32_t)process4->stack_ptr - (uint32_t)process4->stack_top;
+    p4->eax = (uint32_t)0;
+    p4->ebp = (uint32_t)process4->stack_top;
     p4->eip = (uint32_t)proc4;
     p4->eflags = (uint32_t)0x0202;
     p4->cs = (uint32_t)0x08;
@@ -86,8 +86,8 @@ void loadr3(){
 
     pcb *process5 = pcb_setup("proc5", 1, 4);
     context *p5 = (context*)(process5->stack_top);
-    p5->eax = (uint32_t)process5->stack_ptr;
-    p5->ebp = (uint32_t)process5->stack_ptr - (uint32_t)process5->stack_top;
+    p5->eax = (uint32_t)0;
+    p5->ebp = (uint32_t)process5->stack_top;
     p5->eip = (uint32_t)proc5;
     p5->eflags = (uint32_t)0x0202;
     p5->cs = (uint32_t)0x08;
