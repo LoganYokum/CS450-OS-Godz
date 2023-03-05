21e761f6ecd0931a68f311f7c43f6ca3845a1146
Nathan Fielding <nwf00001@mix.wvu.edu>
2023-03-05 17:49:48 -0500

    still debugging sys_call

--- kernel/call.c
+++ kernel/call.c
@@ -1,10 +1,10 @@
 #include <stddef.h>
 #include <sys_req.h>
+#include <pcb_user.h>
 #include <mpx/call.h>
 #include <mpx/pcb.h>
-#include <pcb_user.h>
 
-pcb *executing_process;
+pcb *current_process;
 context *first_context;
 
 context *sys_call(context *c) {
@@ -23,14 +23,18 @@ context *sys_call(context *c) {
         next_process = ready_head; // get head of ready queue
         pcb_remove(next_process);  // and remove it
 
-        executing_process->stack_ptr = (char *) c; // update stack pointer
-        pcb_insert(executing_process); // insert current pcb back into queue
+        if (current_process == NULL) {
+            current_process = next_process;
+        }else {
+            current_process->stack_ptr = (char *) c; // update stack pointer
+            pcb_insert(current_process); // insert current pcb back into queue
+        }
         
         next_context = (context *) next_process->stack_ptr; // get context of next process
         next_context->eax = 0;
         return next_context;
     }else if (op == EXIT) {
-        pcb_free(executing_process); // delete current process
+        pcb_free(current_process); // delete current process
         if (ready_head == NULL) {
             first_context->eax = 0;
             return first_context;
@@ -38,8 +42,12 @@ context *sys_call(context *c) {
         next_process = ready_head; // get head of ready queue
         pcb_remove(next_process);  // and remove it
 
-        executing_process->stack_ptr = (char *) c; // update stack pointer
-        pcb_insert(executing_process); // insert current pcb back into queue
+        if (current_process == NULL) {
+            current_process = next_process;
+        }else {
+            current_process->stack_ptr = (char *) c; // update stack pointer
+            pcb_insert(current_process); // insert current pcb back into queue
+        }
             
         next_context = (context *) next_process->stack_ptr; // get context of next process
         next_context->eax = 0;
