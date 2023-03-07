fa76442731e48cd0f2a72ff8c0fa8baaaf114636
Logan Yokum <lgy00001@mix.wvu.edu>
2023-03-07 13:50:35 -0500

    Removed yeild from help.

--- user/help.c
+++ user/help.c
@@ -14,7 +14,7 @@ void help(char *args){
         println("5. version");
         println("6. pcb");
         println("7. loadr3");
-        println("8. yield");
+        // println("8. yield");
         println("9. alarm");
         return;
     }                          // the param string contains a argument and none extra
@@ -39,9 +39,9 @@ void help(char *args){
     else if(strcmp(args, "loadr3") == 0){     // param = loadr3
         println("*ONLY FOR R3* To use loadr3, simply type loadr3 and the system will load the r3 program.");
     }
-    else if(strcmp(args, "yield") == 0){      // param = yield
-        println("*ONLY FOR R3* To use yield, simply type yield and the system will idle the current process.");
-    }
+    // else if(strcmp(args, "yield") == 0){      // param = yield
+    //     println("*ONLY FOR R3* To use yield, simply type yield and the system will idle the current process.");
+    // }
     else if(strcmp(args, "alarm") == 0){      // param = alarm
         println("To use alarm, type alarm followed by two arguments. Time (when to display the message) and Message (what to display).");
         println("Example: alarm 10:00:00 Time to wake up!");
